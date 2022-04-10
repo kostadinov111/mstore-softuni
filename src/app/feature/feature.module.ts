7af4796715eef877aaa0e38e5d3319eb72d03ad4
Georgi Kostadinov <kostadinov7@gmail.com>
2022-04-10 21:15:13 +0300

    Add Home and About pages

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    AboutComponent
+  ],
   imports: [
     CommonModule
   ]
