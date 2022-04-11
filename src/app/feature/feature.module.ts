ad0d4742b09ee6d85bb487085102a1ecbb26c3f7
Georgi Kostadinov <kostadinov7@gmail.com>
2022-04-11 23:55:26 +0300

    Add artist component-list-item

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ArtistListComponent } from './pages/artist/artist-list/artist-list.component';
+import { ArtistListItemComponent } from './pages/artist/artist-list-item/artist-list-item.component';
 
 
 
@@ -10,7 +11,8 @@ import { ArtistListComponent } from './pages/artist/artist-list/artist-list.comp
   declarations: [
     HomeComponent,
     AboutComponent,
-    ArtistListComponent
+    ArtistListComponent,
+    ArtistListItemComponent
   ],
   imports: [
     CommonModule
