09c002bf3098bf891e99ac4bde20cbed4b2b898d
Georgi Kostadinov <kostadinov7@gmail.com>
2022-04-15 12:33:04 +0300

    Update header component

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -14,20 +14,37 @@ export class HeaderComponent implements OnInit {
   currentUser$: Observable<IUser> = this.authService.currentUser$;
   isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn$;
 
+  private isLoggingOut: boolean = false;
+
   constructor(
     private authService: AuthService,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit(): void {
   }
 
   handleLogout(): void {
-    this.authService.logout$().subscribe(() => {
-      this.router.navigate(['/home']);
+
+    if (this.isLoggingOut) {
+      return;
     }
 
-    )
+    this.isLoggingOut = true;
+
+    this.authService.logout$().subscribe({
+      next: args => {
+        console.log(args);
+        // emitted response
+      },
+      complete: () => {
+        this.isLoggingOut = false;
+        this.router.navigate(['/home']);
+      },
+      error: () => {
+        this.isLoggingOut = false
+      }
+    });
   }
 
 }
